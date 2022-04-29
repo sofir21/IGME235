16fd406ab7ebc4e516394a2a503ddc767b4c71cd
sofir21 <63756067+sofir21@users.noreply.github.com>
2022-04-29 00:29:12 -0400

    Add files via upload

---
+++ project3lvls/Javascript/main.js
@@ -0,0 +1,27 @@
+
+
+//this is going to switch between the two gamemodes, aka the preset levels and the random version
+
+ window.onload = (e) => 
+ {
+    
+     document.querySelector("#gameModeButton1").onclick = loadGameRandom;
+    document.querySelector("#gameModeButton2").onclick = loadGameLevels;
+     
+ }
+
+
+
+
+
+function loadGameRandom()
+{
+    localStorage.setItem('gameState', 1);
+    window.location.href="game.html";
+}
+
+ function loadGameLevels()
+ {
+     localStorage.setItem('gameState', 2);
+     window.location.href="gamelvl.html";
+ }
---
+++ project3lvls/Javascript/modules/square.js
@@ -0,0 +1,48 @@
+class Square {
+    constructor() {
+        this.isVisible = false;
+        this.isBone = false;
+        this.element = null;
+    }
+
+    show() {
+        this.element.classList.add("visible");
+        this.isVisible = true;
+    }
+}
+
+//bones, avoid these blocks
+class Bone extends Square {
+    constructor() {
+        super();
+
+        this.isBone = true;
+        this.value = `<img src='./Images/bone.png'/>`;
+    }
+} 
+
+//just stone block
+class Block extends Square {
+    constructor() {
+        super();
+
+        // count of bones on neighbour squares
+        this.value = 0;
+    }
+
+    increaseValue() {
+        this.value += 1;
+    }
+}
+
+
+class FootPrint extends Square {
+    constructor() {
+        super();
+
+        this.isBone = true;
+        this.value = `<img src='./Images/fprint.png' height= 48px width = 48px>`;
+    }
+} 
+
+export {Block, Bone, FootPrint}
